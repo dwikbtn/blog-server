c9179f0e88545a5d9709fc127f50e4697d3a60e1
dwikbtn <dwikbtn15@gmail.com>
2022-02-01 11:46:54 +0800

    finish the new article

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost/blog");
 const articleRouter = require("./routes/articles");
 app.set("view engine", "ejs");
 
-app.use("/articles", articleRouter);
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   const articles = [
     {
@@ -16,5 +19,7 @@ app.get("/", (req, res) => {
   ];
   res.render("articles/index", { articles: articles });
 });
+
+app.use("/articles", articleRouter);
 
 app.listen(5000);
