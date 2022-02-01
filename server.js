27d16d8262cbbe128557dff7d23574c627745c26
dwikbtn <dwikbtn15@gmail.com>
2022-02-01 14:56:23 +0800

    finish the view

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const Article = require("./models/article");
 const app = express();
 
 mongoose.connect("mongodb://localhost/blog");
@@ -9,17 +9,14 @@ app.set("view engine", "ejs");
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: false }));
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "test article",
-      createdAt: new Date(),
-      description: "Just test for now",
-    },
-  ];
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    date: "desc",
+  });
   res.render("articles/index", { articles: articles });
 });
 
+// this router wiill redirect all to /router and function articleRouter
 app.use("/articles", articleRouter);
 
 app.listen(5000);
